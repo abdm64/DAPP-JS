61cc019c9218b3964b5c50b8b51bfc548a01fea5
Messlleka Abdellah <39844646+abdm64@users.noreply.github.com>
2020-10-14 23:33:55 +0100

    fix timing issue

--- app/src/config/keys.js
+++ app/src/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     account : process.env.ACCOUNT || '0x2bD5AD3F1BD8C96463eA834f110251fD1d5Fc560' ,
     amount : process.env.AMOUNT ||  5 ,
-    time : process.env.TIME  || '17:50' ,
+    time : process.env.TIME  || '21:18' ,
     ifura_ID: process.env.INFURA_ID || "b34ec3bc8dd346eb94f3337e1c9f7cb6"
   };
--- app/src/helper/utils.js
+++ app/src/helper/utils.js
@@ -1,12 +1,18 @@
 
-
-
-
 exports.getCronFromTime = (time) => {
+    const d = new Date()
+    const d1 = new Date()
     const timeStr = time.toString()
     const hours = timeStr.split(":")[0]
     const min = timeStr.split(":")[1]
-
-  
-    return `${min} ${hours} * * *`
+    // convert UTC Time to local Time 
+    const utc_offeset = d.getTimezoneOffset()
+    let utc_date = new Date( d.setMinutes(d.getMinutes() + utc_offeset)) 
+    
+    const  deff_hours =  d1.getHours() - utc_date.getHours()
+     const actuelHour =  deff_hours + parseInt(hours)
+   
+    return `${min} ${actuelHour} * * *`
 }
+
+
